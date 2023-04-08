e94365533e9c8f41bb8859fd8cde894a478cf1ca
Christian <christian.avione.io>
2023-04-08 16:36:59 +0100

    added types and incorporated socket.io

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 import express, { Express, Request, Response } from "express";
+import { Server } from "socket.io";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+io.on("connection", (socket) => {
+  console.log(`[cs] connected`);
 });
 
 app.listen(port, () => {
