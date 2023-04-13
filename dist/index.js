80ae5fcba1071fbc7722b7158e42bffee5e65144
christian-2009 <85346878+christian-2009@users.noreply.github.com>
2023-04-13 23:34:26 +0100

    Allowing users to talk to eachother (#3)

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const http_1 = __importDefault(require("http"));
+const harperSaveMessage_1 = __importDefault(require("./services/harperSaveMessage"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
@@ -26,7 +27,6 @@ app.get("/", (req, res) => {
 let chatRoom = "";
 const allUsers = [];
 io.on("connection", (socket) => {
-    console.log(`[cs] connected, ${socket.id}`);
     socket.on("join_room", (data) => {
         const { username, room } = data;
         chatRoom = room;
@@ -46,6 +46,12 @@ io.on("connection", (socket) => {
             username: "Chat",
             currentTime,
         });
+    });
+    socket.on("send_message", (data) => {
+        var _a;
+        const { message, username, room, createdTime } = data;
+        io.in(room).emit("receive_message", data);
+        (_a = (0, harperSaveMessage_1.default)({ message, username, room, createdTime })) === null || _a === void 0 ? void 0 : _a.then((response) => console.log(`[cs] response`, response)).catch((error) => console.log(`[cs] error`, error));
     });
 });
 server.listen(port, () => {
