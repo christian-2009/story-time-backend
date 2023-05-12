c5afa70ea6d568d300c33033ebb1ae6521534207
christian-2009 <85346878+christian-2009@users.noreply.github.com>
2023-05-12 09:24:44 +0100

    added a rest endpoint to check password is correct (#7)

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: false,
+    trailingComma: 'all',
+    tabWidth: 4,
+};
--- index.ts
+++ index.ts
@@ -4,12 +4,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import http from "http";
 import {
-  allUsersType,
-  ClientToServerDataInterface,
-  ClientToServerEvents,
-  InterServerEvents,
-  ServerToClientEvents,
-  SocketData,
+    allUsersType,
+    ClientToServerDataInterface,
+    ClientToServerEvents,
+    InterServerEvents,
+    ServerToClientEvents,
+    SocketData,
 } from "./types";
 import harperSaveMessage from "./services/harperSaveMessage";
 import harperGetMessages from "./services/harperGetMessages";
@@ -25,93 +25,99 @@ const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 const io = new Server<
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
 >(server, {
-  // allowEIO3: true,
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    // allowEIO3: true,
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
 });
 
 const CHAT_BOT = "ChatBot";
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Servers!");
+    res.send("Express + TypeScript Servers!");
+});
+
+app.get("/:room/:password", async (req: Request, res: Response) => {
+    const room = req.params.room;
+    const password = req.params.password;
+    console.log(`[cs] room, id`, room, password);
+
+    try {
+        await harperHandleTable({ room, password });
+        res.status(200).send(true);
+    } catch (e: any) {
+        res.status(401).send(false);
+    }
 });
 
 let chatRoom = "";
 let allUsers: allUsersType[] = [];
 
-io.on("connection", (socket) => {
-  console.log(`[cs] User connected ${socket.id}`);
+io.on("connection", socket => {
+    console.log(`[cs] User connected ${socket.id}`);
 
-  socket.on("connect_error", (err) => {
-    throw err;
-  });
-  socket.on("join_room", async (data) => {
-    const { username, room, password } = data;
+    socket.on("connect_error", err => {
+        throw err;
+    });
+    socket.on("join_room", async data => {
+        const { username, room, password } = data;
 
-    try {
-      await harperHandleTable({ room, password });
-    } catch (e: any) {
-      io.in(socket.id).emit("error", e.message);
-      throw e;
-    }
+        chatRoom = room;
+        allUsers.push({ id: socket.id, username, room });
+        const chatRoomUsers = allUsers.filter(user => user.room === room);
 
-    chatRoom = room;
-    allUsers.push({ id: socket.id, username, room });
-    const chatRoomUsers = allUsers.filter((user) => user.room === room);
+        socket.to(room).emit("chatroom_users", chatRoomUsers);
+        socket.emit("chatroom_users", chatRoomUsers);
 
-    socket.to(room).emit("chatroom_users", chatRoomUsers);
-    socket.emit("chatroom_users", chatRoomUsers);
+        harperGetMessages(room)
+            ?.then(last100messages => {
+                socket.emit("last_100_messages", last100messages);
+            })
+            .catch(err => console.log(`[cs] err`, err));
 
-    harperGetMessages(room)
-      ?.then((last100messages) => {
-        socket.emit("last_100_messages", last100messages);
-      })
-      .catch((err) => console.log(`[cs] err`, err));
+        socket.join(room);
 
-    socket.join(room);
+        const currentTime = Date.now();
 
-    const currentTime = Date.now();
+        socket.emit("receive_message", {
+            message: `Welcome ${username}`,
+            username: CHAT_BOT,
+            currentTime,
+        });
 
-    socket.emit("receive_message", {
-      message: `Welcome ${username}`,
-      username: CHAT_BOT,
-      currentTime,
+        socket.to(room).emit("receive_message", {
+            message: `${username} has joined the room`,
+            username: CHAT_BOT,
+            currentTime,
+        });
+    });
+    socket.on("send_message", (data: ClientToServerDataInterface) => {
+        const { message, username, room, __createdtime__ } = data;
+        io.in(room).emit("receive_message", data);
+        harperSaveMessage({ message, username, room, __createdtime__ })
+            ?.then(response => console.log(`[cs] response`, response))
+            .catch(error => console.log(`[cs] error`, error));
     });
 
-    socket.to(room).emit("receive_message", {
-      message: `${username} has joined the room`,
-      username: CHAT_BOT,
-      currentTime,
+    socket.on("leave_room", data => {
+        const { username, room } = data;
+        socket.leave(room);
+        const __createdtime__ = Date.now();
+        allUsers = leaveRoom(socket.id, allUsers);
+        socket.to(room).emit("receive_message", {
+            message: `${username} has left the room`,
+            username: CHAT_BOT,
+            __createdtime__,
+        });
     });
-  });
-  socket.on("send_message", (data: ClientToServerDataInterface) => {
-    const { message, username, room, __createdtime__ } = data;
-    io.in(room).emit("receive_message", data);
-    harperSaveMessage({ message, username, room, __createdtime__ })
-      ?.then((response) => console.log(`[cs] response`, response))
-      .catch((error) => console.log(`[cs] error`, error));
-  });
-
-  socket.on("leave_room", (data) => {
-    const { username, room } = data;
-    socket.leave(room);
-    const __createdtime__ = Date.now();
-    allUsers = leaveRoom(socket.id, allUsers);
-    socket.to(room).emit("receive_message", {
-      message: `${username} has left the room`,
-      username: CHAT_BOT,
-      __createdtime__,
-    });
-  });
 });
 
 server.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
